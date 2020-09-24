8382841300a5b7ad73f5b9454d68bafa1272c770
Eloisa Sayuri Tachibana <sayuritachibana.sb@gmail.com>
2020-09-24 13:25:31 -0300

    add o dontenv para alterar a port

--- index.js
+++ index.js
@@ -29,8 +29,10 @@ app.post("/orders", function (req, res) {
   res.end("yes");
 });
 
-app.listen(3000, function () {
-  console.log("listening on port 3000!");
+const port = process.env.PORT ||3000;
+
+app.listen(port, function () {
+  console.log(`listening on port ${port}!`);
 });
 
 function montaBodyVtex(order) {
